3f9595521d6510f2e56e1cd1258e38df36f87fa3
franza <nico.franza@gmail.com>
2021-05-21 02:21:48 +0200

    Storage covered with tests

--- src/storage.rs
+++ src/storage.rs
@@ -1,5 +1,6 @@
 use std::fmt;
 
+#[derive(Debug, Eq, PartialEq)]
 pub struct Error(String);
 
 impl Error {
@@ -14,14 +15,14 @@ impl fmt::Display for Error {
     }
 }
 
-pub trait Storage {
-    fn lookup(&self, bucket: &str, key: &str) -> Result<String, Error>;
+pub trait Storage<T> {
+    fn lookup(&self, bucket: &str, key: &str) -> Result<T, Error>;
     fn store(&self, bucket: &str, key: &str, value: &str) -> Result<(), Error>;
     fn delete(&self, bucket: &str, key: &str) -> Result<(), Error>;
 }
 
 pub mod sqlite {
-    use rusqlite::{Connection, named_params, params};
+    use rusqlite::{Connection, named_params, params, Row};
     use super::{Storage, Error};
     use std::time::{SystemTime, UNIX_EPOCH};
 
@@ -35,11 +36,32 @@ pub mod sqlite {
         }
     }
 
+    #[derive(Eq, PartialEq)]
+    pub struct Entry {
+        pub bucket: String,
+        pub key: String,
+        pub value: String,
+        pub created_on: u64,
+        pub modified_on: Option<u64>,
+    }
+
+    impl Entry {
+        fn from_row(row: &Row) -> Result<Entry, Error> {
+            let bucket = row.get("bucket")?;
+            let key = row.get("key")?;
+            let value = row.get("value")?;
+            let created_on = row.get("created_on")?;
+            let modified_on = row.get("modified_on")?;
+
+            Ok(Entry { bucket, key, value, created_on, modified_on })
+        }
+    }
+
     pub fn setup(filename: &str) -> Result<SqliteStorage, Error> {
         let conn = Connection::open(filename)?;
 
         conn.execute(
-            "create table entries if not exist (
+            "create table entries (
                 id          integer primary key,
                 bucket      text not null,
                 key         text not null,
@@ -54,21 +76,19 @@ pub mod sqlite {
 
     pub fn reset(filename: &str) -> Result<(), Error> {
         let conn = Connection::open(filename)?;
-        conn.execute("truncate table entries", [])?;
+        conn.execute("drop table entries", [])?;
         Ok(())
     }
 
-    impl Storage for SqliteStorage {
-        fn lookup(&self, bucket: &str, key: &str) -> Result<String, Error> {
+    impl Storage<Entry> for SqliteStorage {
+        fn lookup(&self, bucket: &str, key: &str) -> Result<Entry, Error> {
             let mut stmt = self.conn.prepare(
                 "select * from entries where bucket = ?1 and key = ?2"
             )?;
 
             let mut rows = stmt.query(params![bucket, key])?;
             let row = rows.next()?.ok_or(Error::entry_not_found())?;
-            let value: String = row.get("value")?;
-
-            Ok(value)
+            Entry::from_row(row)
         }
 
         fn store(&self, bucket: &str, key: &str, value: &str) -> Result<(), Error> {
---
+++ tests/storage.rs
@@ -0,0 +1,118 @@
+use tresor::storage::{Storage, Error};
+use tresor::storage::sqlite;
+use tresor::storage::sqlite::*;
+
+#[macro_use]
+extern crate serial_test;
+
+fn setup() {
+    reset("test.db").or::<Error>(Ok(())).unwrap();
+}
+
+fn teardown() {
+    reset("test.db").or::<Error>(Ok(())).unwrap();
+}
+
+#[test]
+#[serial]
+fn lookup_nothing() {
+    setup();
+
+    let storage = sqlite::setup("test.db").unwrap();
+    let result = storage.lookup("undefined", "undefined");
+
+    assert!(matches!(result, Err(_)));
+
+    teardown();
+}
+
+#[test]
+#[serial]
+fn insert_entry() {
+    setup();
+
+    let storage = sqlite::setup("test.db").unwrap();
+    let result = storage.store("b", "k", "v");
+
+    assert_eq!(result, Ok(()));
+    teardown();
+}
+
+#[test]
+#[serial]
+fn lookup_entry() {
+    setup();
+
+    let storage = sqlite::setup("test.db").unwrap();
+    let bucket = "b";
+    let key = "k";
+    let value = "v";
+    storage.store(bucket, key, value).unwrap();
+    let result = storage.lookup(bucket, key);
+
+    assert!(matches!(result, Ok(_)));
+
+    let entry = result.unwrap();
+
+    assert_eq!(entry.bucket, bucket.to_string());
+    assert_eq!(entry.key, key.to_string());
+    assert_eq!(entry.value, value.to_string());
+    assert_eq!(entry.modified_on, None);
+
+    teardown();
+}
+
+#[test]
+#[serial]
+fn update_entry() {
+    setup();
+
+    let storage = sqlite::setup("test.db").unwrap();
+    let bucket = "b";
+    let key = "k";
+    let value1 = "v1";
+    storage.store(bucket, key, value1).unwrap();
+    let entry = storage.lookup(bucket, key).unwrap();
+
+    assert_eq!(entry.modified_on, None);
+
+    let value2 = "v2";
+    storage.store(bucket, key, value2).unwrap();
+    let entry = storage.lookup(bucket, key).unwrap();
+
+    assert_eq!(entry.value, value2.to_string());
+    assert!(matches!(entry.modified_on, Some(_)));
+
+    teardown();
+}
+
+#[test]
+#[serial]
+fn delete_non_existent_entry() {
+    setup();
+
+    let storage = sqlite::setup("test.db").unwrap();
+    let bucket = "b";
+    let key = "k";
+    let result = storage.delete(bucket, key);
+
+    assert!(matches!(result, Ok(_)));
+    teardown();
+}
+
+#[test]
+#[serial]
+fn delete_entry() {
+    setup();
+
+    let storage = sqlite::setup("test.db").unwrap();
+    let bucket = "b";
+    let key = "k";
+    let value1 = "v1";
+    storage.store(bucket, key, value1).unwrap();
+
+    let result = storage.delete(bucket, key);
+
+    assert!(matches!(result, Ok(_)));
+    teardown();
+}
